6715eccd3493462b520d2b77e70085aab69f13d8
lakomkaxd <dokukinstepans@gmail.com>
2023-04-24 09:38:49 +0300

    adding dist

--- README.md
+++
@@ -1,2 +0,0 @@
-# todo-app
- Vue todo app
--- src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const projectUrl = import.meta.env.VITE_PROJECT_URL;
-const anonKey = import.meta.env.VITE_ANON_KEY;
-
-export const supabase = createClient(projectUrl, anonKey);
